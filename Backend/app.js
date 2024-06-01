8c5c41fc040af4c768281814122da8be3ac53639
divspark <divyansh2004mhj@gmail.com>
2024-06-01 18:35:26 +0530

    app.js last one

--- Backend/app.js
+++ Backend/app.js
@@ -47,7 +47,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
     origin: "https://grow-frontend-lime.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST","DELETE"],
     optionsSuccessStatus: 200,
     credentials: true
 
